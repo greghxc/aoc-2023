9a12d91cd58a8e180340514ce7153c52e9657a09
greghxc <greg@hacksy.io>
2023-11-30 22:02:34 -0800

    python day 01

--- python/day_01/day_01.py
+++ python/day_01/day_01.py
@@ -1,14 +1,34 @@
+import re
+
 import file_util
+
+DIGITS: list[str] = [
+    "zero", "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine"
+]
+DIGIT_VALUES: dict[str, str] = {v: str(i) for i, v in enumerate(DIGITS)}
 
 
 def part_one(file_name: str) -> int:
-    lines = file_util.get_all_lines_as_int(file_name)
-    return 0
+    def get_number(line: str) -> int:
+        found = re.findall("\\d", line)
+        return int(found[0] + found[-1])
+
+    lines = file_util.get_all_lines(file_name)
+    return sum([get_number(line) for line in lines])
 
 
 def part_two(file_name: str):
-    lines = file_util.get_all_lines_as_int(file_name)
-    return 0
+    def get_number(line: str) -> int:
+        regex = f"(?=(\\d|{'|'.join(DIGITS)}))"
+        found = re.findall(regex, line)
+        first_and_last = found[0] + found[-1]
+        for key in DIGIT_VALUES.keys():
+            first_and_last = first_and_last.replace(key, DIGIT_VALUES[key])
+        return int(first_and_last)
+
+    lines = file_util.get_all_lines(file_name)
+    return sum([get_number(line) for line in lines])
 
 
 if __name__ == "__main__":
--- python/day_01/test_day_01.py
+++ python/day_01/test_day_01.py
@@ -4,7 +4,7 @@ import day_01
 
 class TestDay00(TestCase):
     def test_part_one(self):
-        self.assertEquals(day_01.part_one("input/test1.txt"), 0)
+        self.assertEqual(day_01.part_one("input/test1.txt"), 142)
 
     def test_part_two(self):
-        self.assertEquals(day_01.part_two("input/test1.txt"), 0)
+        self.assertEqual(day_01.part_two("input/test2.txt"), 281)
